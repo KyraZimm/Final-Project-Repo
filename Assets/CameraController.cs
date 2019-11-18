c3bcecd481c2cdc64f731c778eb17ac87bdc718e
kyraz <kyra.zimmermann2017@gmail.com>
2019-11-18 11:39:01 -0500

    committed all

---
+++ Assets/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    /*
+    public GameObject Player;
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - Player.transform.position;
+    }
+
+    private void Update()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Player.transform.position + offset;
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, 0, 64.65f),
+            Mathf.Clamp(transform.position.y, 0, 0),
+            Mathf.Clamp(transform.position.z, -10, -10));
+    }
+    */
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+	
+	//player attributes
+	private static float PlayerSpeed;
+	private static Vector2 Direction;
+	public static bool moveUp;
+	
+	//map attributes
+	//set this as a reference later, instead of manual value
+	public static float MapBoundRight;
+	public static float MapBoundLeft;
+
+	
+	void Start () {
+		
+		//set idle values
+		PlayerSpeed = 10;
+			//note - change these values once placeholders are replaced. Set to reference map sprite.
+			MapBoundLeft = -40;
+			MapBoundRight = 40;
+
+	}
+	
+
+	void Update () {
+		
+		CheckSpeed();
+		
+	}
+
+	void CheckSpeed()
+	{
+		
+		//check for left/right key input
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			Direction = Vector2.right;
+		}
+		else if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			Direction = Vector2.left;
+		}
+		else
+		{
+			Direction = new Vector2(0, 0);
+		}
+		
+		//"up" function--when standing next to ladder/stairs, player can go up
+		if (moveUp)
+		{
+			Direction = Vector2.up;
+		}
+		
+		//check player boundaries
+		if (transform.position.x >= MapBoundRight || transform.position.x <= MapBoundLeft)
+		{
+			Direction = new Vector2(0, 0);
+		}
+
+		transform.Translate(Direction * PlayerSpeed * Time.deltaTime);
+		
+	}
+	
+
+}
