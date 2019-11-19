c5bddf58ddeaa7eb7a9db8e5fdbdc6e89102c0a3
kyraz <kyra.zimmermann2017@gmail.com>
2019-11-19 15:00:28 -0500

    replaced placeholder player prefab with Kea's prefabs working interactable placeholders

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
 	
-	//player attributes
+	//attached components
+	public Rigidbody2D rb;
+	public Animator anim;
+	public SpriteRenderer sprite;
+	
+	//movement
 	private static float PlayerSpeed;
 	private static Vector2 Direction;
 	public static bool moveUp;
@@ -23,6 +28,9 @@ public class PlayerScript : MonoBehaviour {
 			//note - change these values once placeholders are replaced. Set to reference map sprite.
 			MapBoundLeft = -40;
 			MapBoundRight = 40;
+			
+		//reference components
+		
 
 	}
 	
@@ -39,21 +47,37 @@ public class PlayerScript : MonoBehaviour {
 		//check for left/right key input
 		if (Input.GetKey(KeyCode.RightArrow))
 		{
+			//set direction
 			Direction = Vector2.right;
+			
+			//animation
+			sprite.flipX = false;
+			anim.SetBool("IsWalking", true);
 		}
 		else if (Input.GetKey(KeyCode.LeftArrow))
 		{
+			//set direction
 			Direction = Vector2.left;
+			
+			//animation
+			sprite.flipX = true;
+			anim.SetBool("IsWalking", true);
 		}
 		else
 		{
+			//idle
 			Direction = new Vector2(0, 0);
+			anim.SetBool("IsWalking", false);
 		}
 		
 		//"up" function--when standing next to ladder/stairs, player can go up
 		if (moveUp)
 		{
-			Direction = Vector2.up;
+			if (Input.GetKey(KeyCode.Space))
+			{
+				Direction = Vector2.up;
+			}
+			
 		}
 		
 		//check player boundaries
