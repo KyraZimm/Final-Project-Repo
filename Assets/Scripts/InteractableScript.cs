5aba67d5c92a751d15218742a44b3bbccb2fdee0
kyraz <kyra.zimmermann2017@gmail.com>
2019-11-19 15:00:44 -0500

    replaced placeholder player prefab with Kea's prefabs working interactable placeholders

---
+++ Assets/Scripts/InteractableScript.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableScript : MonoBehaviour
+{
+
+    private bool interact;
+
+    void Start()
+    {
+        //set initial value
+        interact = false;
+    }
+
+   
+    void Update()
+    {
+        //allow interaction
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (interact)
+            {
+                Debug.Log("interaction happens!");
+                //create event that occurs
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            interact = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            interact = false;
+        }
+    }
+}
