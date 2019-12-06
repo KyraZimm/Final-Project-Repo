bc7e6b9e62e157425929f14f807573f39ecafc1f
kyraz <kyra.zimmermann2017@gmail.com>
2019-12-05 22:51:18 -0500

    adding computer scene in bedroom

--- Assets/Scripts/InteractableS/Bedroom_Computer.cs
+++ Assets/Scripts/InteractableS/Bedroom_Computer.cs
@@ -1,18 +1,75 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
 
 public class Bedroom_Computer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //UI
+    public TMP_Text RealityText;
+    public TMP_Text FantasyText;
+
+    //update set
+    private int currentSet;
+    private bool CheckForUpdate;
+    
+    //JSON
+    public TextAsset JsonFile;
+    private UIText CanvasText;
+    
+    [Serializable]
+    public struct UIText
     {
-        
+        public string[] Reality;
+        public string[] Fantasy;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        currentSet = -1;
+        CanvasText = JsonUtility.FromJson<UIText>(JsonFile.text);
+        CheckForUpdate = false;
+    }
+
+    private void Update()
+    {
+        if (CheckForUpdate)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                
+            }
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            CheckForUpdate = true;
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            CheckForUpdate = false;
+        }
+    }
+
+    void UpdateText()
+    {
+        var nextSet = currentSet + 1;
+
+        RealityText.text = CanvasText.Reality[nextSet];
+        FantasyText.text = CanvasText.Fantasy[nextSet];
+
+        currentSet = nextSet;
+
     }
 }
