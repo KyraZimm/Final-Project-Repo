17f832a88a3882cfe35fbf57db89b56b9ce81cad
kyraz <kyra.zimmermann2017@gmail.com>
2019-12-06 20:41:11 -0500

    added third hallway conversation with jenny

---
+++ Assets/Scripts/StartButtonScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartButtonScript : MonoBehaviour
+{
+    public Button StartButton;
+    
+    void Start()
+    {
+        StartButton = GetComponent<Button>();
+        StartButton.onClick.AddListener(OnClick);
+    }
+
+    void OnClick()
+    {
+        PlayerPrefs.SetInt("Scene", 0);
+        SceneManager.LoadScene("Scenes/HallwayScene");
+    }
+}
