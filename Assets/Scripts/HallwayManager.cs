3ff3a48b89581098883ecb24a62ab232c164ebf3
kyraz <kyra.zimmermann2017@gmail.com>
2019-12-08 23:15:05 -0500

    debugging - continuing to fix scene updates

---
+++ Assets/Scripts/HallwayManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HallwayManager : MonoBehaviour
+{
+
+    public GameObject playerR;
+    public GameObject playerF;
+    
+    //scene 0
+    public DadScript dadConvo;
+    public HallwayDialogue_Jenny jennyConvo;
+    
+    //scene 2
+    public DadScript2 dadConvo2;
+    public HallwayDialogue_2 jennyConvo2;
+   
+    void Start()
+    {
+        //mark door that Carey walks out of
+        if (PlayerPrefs.GetInt("LastDoorUsed") == 1)
+        {
+            playerR.transform.position = new Vector2(-40, 11.9f);
+            playerF.transform.position = new Vector2(-40, -13.4f);
+        }
+        else if (PlayerPrefs.GetInt("LastDoorUsed") == 2)
+        {
+            playerR.transform.position = new Vector2(-10, 11.9f);
+            playerF.transform.position = new Vector2(-10, -13.4f);
+        }
+        else if (PlayerPrefs.GetInt("LastDoorUsed") == 3)
+        {
+            playerR.transform.position = new Vector2(40, 11.9f);
+            playerF.transform.position = new Vector2(40, -13.4f);
+        }
+    }
+
+    void Update()
+    {
+        if (PlayerPrefs.GetInt("Scene") == 0)
+        {
+            if (dadConvo.interactionOver && jennyConvo.interactionOver)
+            {
+                PlayerPrefs.SetInt("Scene", 1);
+            }
+        }
+        else if (PlayerPrefs.GetInt("Scene") == 2)
+        {
+            if (dadConvo.interactionOver && jennyConvo.interactionOver)
+            {
+                PlayerPrefs.SetInt("Scene", 3);
+            }
+        }
+        
+    }
+
+}
