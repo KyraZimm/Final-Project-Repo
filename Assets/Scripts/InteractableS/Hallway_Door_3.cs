3c012195dd4475e32063e7f7a8e936b3f732c5b7
kyraz <kyra.zimmermann2017@gmail.com>
2019-12-09 10:30:35 -0500

    added script to open jenny's bedroom

--- Assets/Scripts/InteractableS/Hallway_Door_3.cs
+++ Assets/Scripts/InteractableS/Hallway_Door_3.cs
@@ -15,6 +15,7 @@ public class Hallway_Door_3 : MonoBehaviour
                 if (PlayerPrefs.GetInt("Scene") == 5)
                 {
                     PlayerPrefs.SetInt("LastDoorUsed", 3);
+                    SceneManager.LoadScene("Scenes/JennyBedroom");
                 }
             }
         }
