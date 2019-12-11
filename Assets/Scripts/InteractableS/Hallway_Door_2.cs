b6d371dcc3735099d01dee37a7dff80998b61dfb
kyraz <kyra.zimmermann2017@gmail.com>
2019-12-11 00:34:08 -0500

    merge

--- Assets/Scripts/InteractableS/Hallway_Door_2.cs
+++ Assets/Scripts/InteractableS/Hallway_Door_2.cs
@@ -5,17 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class Hallway_Door_2 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     
     private void OnTriggerStay2D(Collider2D other)
     {
